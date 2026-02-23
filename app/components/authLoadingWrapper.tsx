ee7196918d28bcd25367ce6218e7a0fdffb8461f
geraldman <g3rald.dj@gmail.com>
2026-02-23 22:55:07 +0700

    final changes in main

--- app/components/authLoadingWrapper.tsx
+++ app/components/authLoadingWrapper.tsx
@@ -4,17 +4,15 @@ import LoadingScreen from "@/app/components/LoadingScreenFixed";
 
 type Props = {
   loading: boolean;
-  text?: string;
   children: React.ReactNode;
 };
 
 export default function AuthLoadingWrapper({
   loading,
-  text,
   children,
 }: Props) {
   if (loading) {
-    return <LoadingScreen text={text} />;
+    return <LoadingScreen />;
   }
 
   return <>{children}</>;
